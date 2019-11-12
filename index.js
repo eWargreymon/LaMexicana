44743c263fa46378f69082cd0ba1ba6788ec8806
Adrián Godoy Martínez <adriangodoymartinez@icloud.com>
2019-11-12 12:45:04 +0000

    Login implemented

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+/*
 $(function () {
   $('#datetimepicker1').datetimepicker();
 });
+*/
+
+function getUser ()
+{
+  $.getJSON("./users.json", function (json) {
+    var inputUsername = document.getElementById("username").value;
+    var inputPassword = document.getElementById("password").value;
+
+    for(var user in json) {
+      if (json.hasOwnProperty(user)) {
+        if (json[user].username === inputUsername && json[user].password === inputPassword) {
+          alert("El usuario " + json[user].username + " ha iniciado sesión con exito");
+          window.location.href = "index.html";
+          break
+        } else {
+          alert("Usuario o contraseña incorrecto, inténtelo de nuevo");
+          break
+        }
+      }
+    }
+  });
+}
