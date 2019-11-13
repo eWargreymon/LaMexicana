398fb323fde6edf680aab768112e22f05347d625
Adrián Godoy Martínez <adriangodoymartinez@icloud.com>
2019-11-13 15:19:34 +0000

    Login and log out implemented

--- index.js
+++ index.js
@@ -4,23 +4,44 @@ $(function () {
 });
 */
 
-function getUser ()
-{
-  $.getJSON("./users.json", function (json) {
-    var inputUsername = document.getElementById("username").value;
-    var inputPassword = document.getElementById("password").value;
+$(document).ready(function () {
+  if(sessionStorage.length > 0) {
+    $("li:has('a'):contains('Login')").remove();
+    $(".navbar-nav").append('<li class="nav-item"><a class="nav-link" href="index.html" onclick="logOut();">Log out</a></li>');
+  }
 
-    for(var user in json) {
-      if (json.hasOwnProperty(user)) {
-        if (json[user].username === inputUsername && json[user].password === inputPassword) {
-          alert("El usuario " + json[user].username + " ha iniciado sesión con exito");
-          window.location.href = "index.html";
-          break
-        } else {
-          alert("Usuario o contraseña incorrecto, inténtelo de nuevo");
-          break
+});
+
+function getUser() {
+    $.getJSON("./users.json", function (json) {
+        var inputUsername = document.getElementById("username").value;
+        var inputPassword = document.getElementById("password").value;
+
+        for (var user in json) {
+            if (json.hasOwnProperty(user)) {
+                if (json[user].username === inputUsername && json[user].password === inputPassword) {
+                    alert("El usuario " + json[user].username + " ha iniciado sesión con exito");
+                    saveUser(inputUsername);
+                    window.location.href = "index.html";
+                    break
+                } else {
+                    alert("Usuario o contraseña incorrecto, inténtelo de nuevo");
+                    break
+                }
+            }
         }
-      }
-    }
-  });
+    });
+}
+
+
+function saveUser(userName) {
+    sessionStorage.setItem("user", userName)
+}
+
+function getUserLogged() {
+    alert("El usario registrado es: " + sessionStorage.getItem("user").valueOf());
+}
+
+function logOut() {
+    sessionStorage.clear();
 }
