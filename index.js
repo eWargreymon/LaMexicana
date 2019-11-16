2faf055d029f501663dc92c4896b313c2d67a04a
Adrián Godoy Martínez <adriangodoymartinez@icloud.com>
2019-11-16 15:20:53 +0000

    Login implemented

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ $(function () {
 */
 
 $(document).ready(function () {
-  if(sessionStorage.length > 0) {
-    $("li:has('a'):contains('Login')").remove();
-    $(".navbar-nav").append('<li class="nav-item"><a class="nav-link" href="index.html" onclick="logOut();">Log out</a></li>');
-  }
+    if (sessionStorage.length > 0) {
+        $("li:has('a'):contains('Login')").remove();
+        $(".navbar-nav").append('<li class="nav-item"><a class="nav-link" href="index.html" onclick="logOut();">Log out</a></li>');
+    }
 
 });
 
-function getUser() {
+function login() {
     $.getJSON("./users.json", function (json) {
         var inputUsername = document.getElementById("username").value;
         var inputPassword = document.getElementById("password").value;
@@ -20,7 +20,7 @@ function getUser() {
         for (var user in json) {
             if (json.hasOwnProperty(user)) {
                 if (json[user].username === inputUsername && json[user].password === inputPassword) {
-                    alert("El usuario " + json[user].username + " ha iniciado sesión con exito");
+                    //alert("El usuario " + json[user].username + " ha iniciado sesión con exito");
                     saveUser(inputUsername);
                     window.location.href = "index.html";
                     break
@@ -39,9 +39,12 @@ function saveUser(userName) {
 }
 
 function getUserLogged() {
-    alert("El usario registrado es: " + sessionStorage.getItem("user").valueOf());
+    return sessionStorage.getItem("user").valueOf();
 }
 
 function logOut() {
     sessionStorage.clear();
 }
+
+
+
